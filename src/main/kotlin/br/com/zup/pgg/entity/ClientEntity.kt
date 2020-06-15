357104d72fd612f8996bbbc3d942c0e90d366a0c
braulio <braulio.carvalho@zup.com.br>
2020-06-15 16:33:45 -0300

    Initial Commit

---
+++ src/main/kotlin/br/com/zup/pgg/entity/ClientEntity.kt
@@ -0,0 +1,13 @@
+package br.com.zup.pgg.entity
+
+data class ClientEntity(
+
+        val nome: String,
+        val idade: Int,
+        val cpf: String,
+        val email: String,
+        val telefone: String,
+        val endereco: String
+
+
+)
---
+++ src/main/kotlin/br/com/zup/pgg/service/ClientService.kt
@@ -0,0 +1,4 @@
+package br.com.zup.pgg.service
+
+class ClientService {
+}
